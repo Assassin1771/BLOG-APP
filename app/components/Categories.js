3c58f59d34592db30a30c5d5b2fd79a1383e11e6
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-14 16:13:15 +0530

    Created components folder

---
+++ app/components/Categories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div>
+      <div>CATEGORY 1 CATEGORY 2</div>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ app/components/PostCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PostCard = ({ post }) => {
+  return (
+    <div>
+      <div>
+        {post.title}
+        {post.excerpt}
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
