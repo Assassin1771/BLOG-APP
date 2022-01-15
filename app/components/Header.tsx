0cb313c1d99d5534fe4f7697120cf877527a47fd
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-16 02:04:36 +0530

    Categories Section Done

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-
 import Link from "next/link";
-// import { getCategories } from '../services';
-
-const categories = [
-  { name: "Tushar", slug: "Garg" },
-  { name: "Kartike", slug: "Tiwari" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
