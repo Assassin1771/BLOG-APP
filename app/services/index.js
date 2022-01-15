f192041c20c1e2fb05d919d8ca321523c1fc1acf
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-15 21:59:07 +0530

    Minor change

--- app/services/index.js
+++ app/services/index.js
@@ -1,3 +1,6 @@
+//This is the file which is used to connect to GRAPHCMS
+//We also also be fetching data using GraphQl by creating a query.
+
 import { request, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
